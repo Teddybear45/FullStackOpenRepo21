67961c0570a7ac9aadf42963c62c3e3b896b9d06
Ted Guan <tedguan2005@gmail.com>
2021-11-24 15:30:18 -0800

    contol flow and err handling. skipping loops and iters

--- part1/Lesson-Content/JS-lesson-trek/MDN-guide-overview/Control-flow/overview.js
+++ part1/Lesson-Content/JS-lesson-trek/MDN-guide-overview/Control-flow/overview.js
@@ -1,6 +1,6 @@
 //falsy values
 // false
-//undefined 
+//undefined
 // null
 // 0
 // ''
@@ -9,4 +9,16 @@
 // ALL objects evaluate as true
 
 //exception handling
+
+// try {
+//     // code that might throw an exception
+// } catch (e) {
+//     // code to handle the exception
+//     for example: logMyErrors(e);
+// }
+
+//can use finally when you want to execute code regardless of whether an exception is thrown
+// example is to close a file after it is opened in the try block
+
+
 
