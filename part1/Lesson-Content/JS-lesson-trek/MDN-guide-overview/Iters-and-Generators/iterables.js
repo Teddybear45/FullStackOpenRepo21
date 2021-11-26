bebdaff6e17f1484e96cccf7a9606899d73214a5
Ted Guan <tedguan2005@gmail.com>
2021-11-25 22:49:45 -0800

    fin iters

--- part1/Lesson-Content/JS-lesson-trek/MDN-guide-overview/Iters-and-Generators/iterables.js
+++ part1/Lesson-Content/JS-lesson-trek/MDN-guide-overview/Iters-and-Generators/iterables.js
@@ -26,3 +26,17 @@ it[Symbol.iterator] = function* () {
 
 //user defined iterables
 
+const myIterable = {
+	*[Symbol.iterator]() {
+		yield 1;
+		yield 2;
+		yield 3;
+	},
+};
+
+for (let value of myIterable) {
+	console.log(value);
+}
+// 1
+// 2
+// 3
