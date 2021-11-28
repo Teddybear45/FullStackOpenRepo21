b52817a4bba16125df4a808e4b78b035510b8e2d
Ted Guan <tedguan2005@gmail.com>
2021-11-27 23:38:17 -0800

    Lesson C. state changing and re-rendering

--- part1/Lesson-Content/C-lesson-notes/my-react-app-lc/src/App.js
+++ part1/Lesson-Content/C-lesson-notes/my-react-app-lc/src/App.js
@@ -1,33 +1,98 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 
-const Hello = (props) => {
-	//destructuring props
-  const { name, age } = props;
-  
-  const bornYear = () => {
-		return new Date().getFullYear() - age;
+// Part 1:
+// const Hello = ({name, age}) => {
+// 	//destructuring props
+//   // const { name, age } = props;
+
+//   const bornYear = () => {
+// 		return new Date().getFullYear() - age;
+// 	};
+
+// 	return (
+// 		<div>
+// 			<p>
+// 				Hello {name}, you are {age} years old
+// 			</p>
+//       <p>So you were probably born in {bornYear()}</p>
+// 		</div>
+// 	);
+// };
+
+// const App = () => {
+// 	const name = "Peter";
+// 	const age = 10;
+
+// 	return (
+// 		<div>
+// 			<h1>Greetings</h1>
+// 			<Hello name="Maya" age={26 + 10} />
+// 			<Hello name={name} age={age} />
+// 		</div>
+// 	);
+// };
+
+//part 2: using reactDOM.render()
+// const App = (props) => {
+//   const {counter} = props
+//   return (
+//     <div>{counter}</div>
+//   )
+// }
+
+// part 3: using stateful components
+
+const App = (props) => {
+	const [counter, setCounter] = useState(0); // takes in 0 as the initial value of counter
+	// useState returns an array with two elements: the current state and a function to update it
+	// we assign the first element of the array to a variable called counter and the second to a function called setCounter
+	// the function setCounter takes in a value and updates the state
+
+	//when the state modifying function setCounter is called, React re-renders the component, in this case the component counter
+
+	//part 3a: using a timeout function
+	// setTimeout(() => {
+	// 	setCounter(counter + 1);
+	// }, 1000);
+
+	// return <div>the current count is: {counter}</div>;
+
+	// part 3b: using a button to increment the counter
+
+	const incrementCounter = () => {
+		setCounter(counter + 1);
 	};
-  
+	const decrementCounter = () => {
+		setCounter(counter - 1);
+	};
+	const resetCounter = () => {
+		setCounter(0);
+	};
+	// return (
+	// 	<div>
+	// 		<div>{counter}</div>
+	// 		<button onClick={incrementCounter}>Add Counter</button>
+	// 		<button onClick={decrementCounter}>Subtract Counter</button>
+	// 		<button onClick={resetCounter}>Reset Counter</button>
+	// 	</div>
+	// );
+
+	//part 4: using many child components
+	const Display = ({counter}) => {
+		return <div>{counter}</div>;
+	};
+
+	const Button = ({onClick, text}) => {
+		return <button onClick={onClick}>{text}</button>;
+	};
+
 	return (
 		<div>
-			<p>
-				Hello {name}, you are {age} years old
-			</p>
-      <p>So you were probably born in {bornYear()}</p>
-		</div>
-	);
-};
-
-const App = () => {
-	const name = "Peter";
-	const age = 10;
-
-	return (
-		<div>
-			<h1>Greetings</h1>
-			<Hello name="Maya" age={26 + 10} />
-			<Hello name={name} age={age} />
+			<Display counter={counter} />
+			<Button onClick={incrementCounter} text="Add Counter" />
+			<Button onClick={decrementCounter} text="Subtract Counter" />
+			<Button onClick={resetCounter} text="Reset Counter" />
 		</div>
 	);
 };
--- part1/Lesson-Content/C-lesson-notes/my-react-app-lc/src/index.js
+++ part1/Lesson-Content/C-lesson-notes/my-react-app-lc/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Part 1
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Part 2: using reactDOM.render()
+// let counter = 1;
+
+// ReactDOM.render(<App counter={counter} />, document.getElementById("root"));
+
+// const refresh = () => {
+// 	ReactDOM.render(<App counter={counter} />, document.getElementById("root"));
+// };
+
+// setInterval(() => {
+// 	refresh();
+// 	counter += 1;
+// }, 1000);
+
+//using reactDOM.render isn't a great way to re-render components
+
+//Part 3: Using states
+
+ReactDOM.render(<App />, 
+  document.getElementById('root'))
+
+
