794b1dec8f04eeca7e708691631c58dfa1df40db
Ted Guan <tedguan2005@gmail.com>
2022-07-01 12:25:48 -0700

    upto 2.19 phonebook s11

--- part2/notes-app/src/App.js
+++ part2/notes-app/src/App.js
@@ -4,14 +4,14 @@ import { useState, useEffect } from "react";
 import Note from "./components/Note";
 import axios from "axios";
 import noteService from "./services/notes";
+import Notification from "./components/Notification";
 
 const App = (props) => {
 	const [notes, setNotes] = useState([]);
-
 	// controlled component
 	const [newNote, setNewNote] = useState("a new note...");
-
 	const [showAll, setShowAll] = useState(true);
+	const [errorMessage, setErrorMessage] = useState(null);
 
 	useEffect(() => {
 		noteService.getAll().then((initialNotes) => setNotes(initialNotes));
@@ -48,14 +48,37 @@ const App = (props) => {
 				setNotes(notes.map((n) => (n.id !== id ? n : returnedNote)));
 			})
 			.catch((error) => {
-				alert(`the note '${note.content}' was already deleted from server`);
+				setErrorMessage(
+					`Note '${note.content}' was already removed from server`
+				);
+				setTimeout(() => {
+					setErrorMessage(null);
+				}, 5000);
 				setNotes(notes.filter((n) => n.id !== id));
 			});
+	};
+
+	const Footer = () => {
+		const footerStyle = {
+			color: "green",
+			fontStyle: "italic",
+			fontSize: 16,
+		};
+		return (
+			<div style={footerStyle}>
+				<br />
+				<em>
+					Note app, Department of Computer Science, University of Helsinki 2022
+				</em>
+			</div>
+		);
 	};
 
 	return (
 		<div>
 			<h1>Notes</h1>
+			<Notification message={errorMessage} />
+
 			<div>
 				<button onClick={() => setShowAll(!showAll)}>
 					show {showAll ? "important" : "all"}
@@ -74,6 +97,7 @@ const App = (props) => {
 				<input value={newNote} onChange={handleNoteChange} />
 				<button type="submit">save/submit</button>
 			</form>
+			<Footer />
 		</div>
 	);
 };
